56f3cb5cf8e73aebae2fa52eb462930511fbe823
SasukeUchiha88 <su739200@gmail.com>
2026-05-09 00:31:46 +0530

    Add translateText function for text translation

--- main.js
+++ main.js
@@ -9,4 +9,11 @@ console.log(result);
 function darkmode(){
   document.body.style.background = "white";
   document.body.style.color = "black";
+}function translateText(){
+
+let text = document.getElementById("text").value;
+
+document.getElementById("result").innerHTML =
+"Translated: " + text;
+
 }
